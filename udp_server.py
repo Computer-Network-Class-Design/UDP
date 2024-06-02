e7d7d9684e75de1f28975e55fd15fe2c57f8689d
JimGu <gwt200407@gmail.com>
2024-06-02 22:40:12 +0800

    Add udp server

---
+++ udp_server.py
@@ -0,0 +1,65 @@
+import socket
+import random
+import datetime
+
+from typing import Tuple
+
+from .config import Settings
+
+
+class UDPServer:
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server.bind((Settings.IP, Settings.PORT))
+
+        self.initial_response = self.final_response = None
+
+    @staticmethod
+    def __emulate_loss() -> bool:
+        return random.random() >= Settings.LOSS
+
+    def _handle_client(self, msg: bytes, addr: Tuple[str, int]):
+        def pad_string(s: str) -> str:
+            if len(s) < Settings.CONTENT:
+                return "".join(s, " " * (Settings.CONTENT - len(s)))
+            return s[Settings.CONTENT]
+
+        curr_time = int(datetime.datetime.now().timestamp())
+
+        msg = msg.decode(Settings.FORMAT)
+        seq, ver, syn, fin, content = (
+            msg[:16],
+            msg[16:24],
+            int(msg[24]),
+            int(msg[25]),
+            msg[26::].strip(),
+        )
+
+        if not syn and not self.initial_response:
+            self.initial_response = curr_time
+        if not syn and not fin:
+            self.final_response = curr_time
+
+        if syn:
+            msg_to_send = "".join(
+                [seq, ver, str(syn), str(fin), pad_string(content)]
+            ).encode(Settings.FORMAT)
+        elif fin:
+            msg_to_send = "".join(
+                [seq, ver, str(syn), str(fin), pad_string(Settings.FIN_ACK)]
+            ).encode(Settings.FORMAT)
+        else:
+            msg_to_send = "".join(
+                [seq, ver, str(syn), str(fin), pad_string(str(curr_time))]
+            ).encode(Settings.FORMAT)
+
+        self.server.sendto(msg_to_send, addr)
+
+        return fin == 1
+
+    def run(self) -> None:
+        while True:
+            msg, addr = self.server.recvfrom(Settings.BUFF_SIZE)
+
+            if UDPServer.__emulate_loss() and self._handle_client(msg, addr):
+                break
